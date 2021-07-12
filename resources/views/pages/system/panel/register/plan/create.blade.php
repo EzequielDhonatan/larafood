bf55eb2dd4bd6ea9736f00e9d8754404c4860818
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-07-12 13:37:02 -0300

    Register - Plan (Breadcrump)

--- resources/views/pages/system/panel/register/plan/create.blade.php
+++ resources/views/pages/system/panel/register/plan/create.blade.php
@@ -6,7 +6,6 @@
         </h2>
     </x-slot>
 
-
     <div class="py-12">
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -14,6 +13,34 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                <div class="row">
+
+                    <div class="col-sm-12 col-xs-12 col-lg-12 col-md-12">
+
+                        <nav aria-label="breadcrumb">
+
+                            <ol class="breadcrumb">
+
+                                <li class="breadcrumb-item">
+                                    <a href="{{ route( 'dashboard' ) }}">Home</a>
+                                </li>
+
+                                <li class="breadcrumb-item">
+                                    <a href="{{ route( 'plan.index' ) }}">Planos</a>
+                                </li>
+
+                                <li class="breadcrumb-item active" aria-current="page">
+                                Dados do plano
+                                </li>
+
+                            </ol> <!-- breadcrumb -->
+
+                        </nav> <!-- breadcrumb -->
+
+                    </div> <!-- -->
+
+                </div> <!-- row -->
 
                     <div class="row">
 

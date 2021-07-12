3d1665f09bb51ac889a94d8397058a9e6b10c38c
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-07-12 14:01:48 -0300

    Register - Plan (Breadcrump)

--- resources/views/pages/system/panel/register/plan/create.blade.php
+++ resources/views/pages/system/panel/register/plan/create.blade.php
@@ -1,9 +1,35 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __( 'Cadastrar novo plano' ) }}
-        </h2>
+
+        <div class="row">
+
+            <div class="col-sm-12 col-xs-12 col-lg-12 col-md-12">
+
+                <nav aria-label="breadcrumb">
+
+                    <ol class="breadcrumb">
+
+                        <li class="breadcrumb-item">
+                            <a href="{{ route( 'dashboard' ) }}">Dashboard</a>
+                        </li>
+
+                        <li class="breadcrumb-item">
+                            <a href="{{ route( 'plan.index' ) }}">Planos</a>
+                        </li>
+
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Dados do plano
+                        </li>
+
+                    </ol> <!-- breadcrumb -->
+
+                </nav> <!-- breadcrumb -->
+
+            </div> <!-- -->
+
+        </div> <!-- row -->
+
     </x-slot>
 
     <div class="py-12">
@@ -13,34 +39,6 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                 <div class="p-6 bg-white border-b border-gray-200">
-
-                    <div class="row">
-
-                        <div class="col-sm-12 col-xs-12 col-lg-12 col-md-12">
-
-                            <nav aria-label="breadcrumb">
-
-                                <ol class="breadcrumb">
-
-                                    <li class="breadcrumb-item">
-                                        <a href="{{ route( 'dashboard' ) }}">Dashboard</a>
-                                    </li>
-
-                                    <li class="breadcrumb-item">
-                                        <a href="{{ route( 'plan.index' ) }}">Planos</a>
-                                    </li>
-
-                                    <li class="breadcrumb-item active" aria-current="page">
-                                        Dados do plano
-                                    </li>
-
-                                </ol> <!-- breadcrumb -->
-
-                            </nav> <!-- breadcrumb -->
-
-                        </div> <!-- -->
-
-                    </div> <!-- row -->
 
                     <div class="row">
 

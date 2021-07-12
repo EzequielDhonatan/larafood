c0398d6b18c74bd6240dc8d02f9336b0be2cfc31
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-07-12 12:18:10 -0300

    Register - Plan (Updated)

--- resources/views/pages/system/panel/register/plan/index.blade.php
+++ resources/views/pages/system/panel/register/plan/index.blade.php
@@ -22,58 +22,62 @@
 
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-                    <table class="table table-hover table-sm">
+                    <div class="table-responsive">
 
-                        <thead>
+                        <table class="table table-hover table-sm">
 
-                            <tr>
+                            <thead>
 
-                                <th scope="col">#</th>
-                                <th scope="col">Nome</th>
-                                <th scope="col">URL</th>
-                                <th scope="col">Preço</th>
-                                <th scope="col"></th>
+                                <tr>
 
-                            </tr> <!-- -->
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">URL</th>
+                                    <th scope="col">Preço</th>
+                                    <th scope="col"></th>
 
-                        </thead> <!-- -->
+                                </tr> <!-- -->
 
-                        <tbody>
+                            </thead> <!-- -->
 
-                        @forelse( $plans as $plan )
+                            <tbody>
 
-                            <tr>
+                            @forelse( $plans as $plan )
 
-                                <th scope="row">{{ $plan->id }}</th>
-                                <td>{{ $plan->name }}</td>
-                                <td>{{ $plan->url }}</td>
-                                <td>R${{ number_format( $plan->price, 2, ',', ' ' ) }}</td>
+                                <tr>
 
-                                <td>
-                                    <a href="#">
-                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </div>
-                                    </a>
-                                </td>
+                                    <th scope="row">{{ $plan->id }}</th>
+                                    <td>{{ $plan->name }}</td>
+                                    <td>{{ $plan->url }}</td>
+                                    <td>R${{ number_format( $plan->price, 2, ',', ' ' ) }}</td>
 
-                            </tr> <!-- -->
+                                    <td>
+                                        <a href="#">
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </td>
 
-                        </tbody> <!-- -->
+                                </tr> <!-- -->
 
-                        {!! $plans->links() !!}
+                            {!! $plans->links() !!}
 
-                        @empty
+                            @empty
 
-                            <div class="text-center">
-                                <h4>Ops... Nenhum registro encontrado!</h4>
-                            </div>
+                                <div class="text-center">
+                                    <h4>Ops... Nenhum registro encontrado!</h4>
+                                </div>
 
-                    </table> <!-- -->
+                            @endforelse
 
-                    @endforelse
+                            </tbody> <!-- -->
+
+                        </table> <!-- -->
+
+                    </div> <!-- table-responsive -->
 
                 </div> <!-- flex flex-col -->
 

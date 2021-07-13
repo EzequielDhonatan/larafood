43e093995991bce6ddee2d44474b429ea0f44742
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-07-13 17:34:39 -0300

    Management - Profile

---
+++ app/Models/System/Panel/Management/Profile/Profile.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\System\Panel\Management\Profile;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+} // Profile
